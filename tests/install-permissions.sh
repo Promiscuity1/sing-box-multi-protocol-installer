70f155236d7b06c5d7dfcb7c3be3dab7aaa90321
Promiscuity1 <Promiscuity1@users.noreply.github.com>
2026-07-14 14:16:15 +0800

    test: mock systemctl on installer path

--- tests/install-permissions.sh
+++ tests/install-permissions.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -eu
 
-mock_dir=$(mktemp -d /root/sb-systemctl.XXXXXX)
-trap 'rm -rf "$mock_dir"' EXIT INT TERM
-cp /bin/true "$mock_dir/systemctl"
 apt-get update
 apt-get install -y --no-install-recommends systemd
 
-PATH="$mock_dir:$PATH" sh ./install.sh --server-address 203.0.113.10
+mock_systemctl=/usr/local/bin/systemctl
+test ! -e "$mock_systemctl"
+trap 'rm -f "$mock_systemctl"' EXIT INT TERM
+cp /bin/true "$mock_systemctl"
+test "$(command -v systemctl)" = "$mock_systemctl"
+
+sh ./install.sh --server-address 203.0.113.10
 
 test "$(stat -c '%U:%G' /etc/sing-box)" = 'root:sing-box'
 su -s /bin/sh -c 'test -r /etc/sing-box/config.json && sing-box check -c /etc/sing-box/config.json -C /etc/sing-box/conf.d' sing-box
